96116cac9791d526b46cbc29723c817bdc542310
Jack Chan <j4ckcyw@users.noreply.github.com>
2021-03-10 22:47:48 -0800

    Add logger
    Command and sample output:
    
    ```
    RUST_LOG=debug cargo test ingress::writer -- --nocapture
    
    [2021-03-11T06:47:27Z DEBUG prototype::ingress::writer::tests] Actual_batch: RecordBatch {
            schema: Schema {
                fields: [
                    Field {
                        name: "a",
                        data_type: Int64,
                        nullable: false,
                        dict_id: 0,
                        dict_is_ordered: false,
                        metadata: None,
                    },
                ],
                metadata: {},
            },
            columns: [
                PrimitiveArray<Int64>
                [
                  1,
                  2,
                  3,
                ],
            ],
        }
    ```

--- src/ingress/writer.rs
+++ src/ingress/writer.rs
@@ -1,10 +1,7 @@
-use arrow::array::{ArrayRef, Int64Array};
-use arrow::datatypes::{DataType, Field, Schema, SchemaRef};
+use arrow::datatypes::SchemaRef;
 use arrow::record_batch::RecordBatch;
-use parquet::arrow::{ArrowReader, ArrowWriter, ParquetFileArrowReader};
-use parquet::file::serialized_reader::{SerializedFileReader, SliceableCursor};
+use parquet::arrow::ArrowWriter;
 use parquet::file::writer::InMemoryWriteableCursor;
-use std::sync::Arc;
 use uuid::Uuid;
 
 pub struct File {
@@ -35,39 +32,57 @@ impl Writer {
     }
 }
 
-#[test]
-fn roundtrip() {
-    let schema = Arc::new(Schema::new(vec![Field::new("a", DataType::Int64, false)]));
+#[cfg(test)]
+mod tests {
+    use super::Writer;
+    use arrow::array::Int64Array;
+    use arrow::datatypes::{DataType, Field, Schema};
+    use arrow::record_batch::RecordBatch;
+    use log::debug;
+    use parquet::arrow::{ArrowReader, ParquetFileArrowReader};
+    use parquet::file::serialized_reader::{SerializedFileReader, SliceableCursor};
+    use std::sync::Arc;
 
-    let a = Arc::new(Int64Array::from(vec![1, 2, 3]));
-    let batch = RecordBatch::try_new(schema.clone(), vec![a.clone()]).unwrap();
+    fn init() {
+        let _ = env_logger::builder().is_test(true).try_init();
+    }
 
-    let writer = Writer::new(schema.clone());
-    let file = writer.write(batch);
+    #[test]
+    fn roundtrip() {
+        init();
+        let schema = Arc::new(Schema::new(vec![Field::new("a", DataType::Int64, false)]));
 
-    let cursor = SliceableCursor::new(file.content);
-    let reader = SerializedFileReader::new(cursor).unwrap();
-    let mut arrow_reader = ParquetFileArrowReader::new(Arc::new(reader));
+        let a = Arc::new(Int64Array::from(vec![1, 2, 3]));
+        let batch = RecordBatch::try_new(schema.clone(), vec![a.clone()]).unwrap();
 
-    let readers = vec![
-        arrow_reader
-            .get_record_reader_by_columns(vec![0], 1024)
-            .unwrap(),
-        arrow_reader.get_record_reader(1024).unwrap(),
-    ];
+        let writer = Writer::new(schema.clone());
+        let file = writer.write(batch);
 
-    for mut reader in readers {
-        let actual_batch = reader
-            .next()
-            .expect("No batch found")
-            .expect("Unable to get batch");
-        assert_eq!(3, actual_batch.num_rows());
-        assert_eq!(1, actual_batch.num_columns());
-        let actual_col = actual_batch
-            .column(0)
-            .as_any()
-            .downcast_ref::<Int64Array>()
-            .unwrap();
-        assert_eq!(*a, *actual_col);
+        let cursor = SliceableCursor::new(file.content);
+        let reader = SerializedFileReader::new(cursor).unwrap();
+        let mut arrow_reader = ParquetFileArrowReader::new(Arc::new(reader));
+
+        let readers = vec![
+            arrow_reader
+                .get_record_reader_by_columns(vec![0], 1024)
+                .unwrap(),
+            arrow_reader.get_record_reader(1024).unwrap(),
+        ];
+
+        for mut reader in readers {
+            let actual_batch = reader
+                .next()
+                .expect("No batch found")
+                .expect("Unable to get batch");
+            debug!("Actual_batch: {:#?}", actual_batch);
+            assert_eq!(3, actual_batch.num_rows());
+            assert_eq!(1, actual_batch.num_columns());
+            let actual_col = actual_batch
+                .column(0)
+                .as_any()
+                .downcast_ref::<Int64Array>()
+                .unwrap();
+            assert_eq!(*a, *actual_col);
+        }
     }
 }
