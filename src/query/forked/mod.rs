6360c66cadada19049ae757e4b609a67f561e56c
Jack Chan <j4ckcyw@users.noreply.github.com>
2021-06-07 22:53:52 -0700

    Add forked parquet table and exec

---
+++ src/query/forked/mod.rs
@@ -0,0 +1,5 @@
+// Forked from github
+// https://github.com/apache/arrow-datafusion/blob/master/datafusion/src/physical_plan/parquet.rs
+// https://github.com/apache/arrow-datafusion/blob/master/datafusion/src/datasource/parquet.rs
+pub mod parquet_exec;
+pub mod parquet_table;
--- src/query/mod.rs
+++ src/query/mod.rs
@@ -1,1 +1,2 @@
+mod forked;
 pub mod query;
